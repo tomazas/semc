3a518b0125b69eada14c80b2645b28d638ecc861
tomazas <qverdi@hotmail.com>
2013-09-21 16:23:50 +0300

    Optimized PCM difference analyzer.

--- Analysis/sq_diff_analyser.m
+++ Analysis/sq_diff_analyser.m
@@ -1,26 +1,23 @@
-% compares PCM streams dirrectly by taking a squared difference of the
+% compares PCM streams dirrectly by taking a difference of the
 % sample data and summing the result as score, lower score is better (0 = PCMs match perfectly) 
 function score = sq_diff_analyser(raw_pcm, enc_pcm)
-    % transcoded PCM is longer, compare only available length
-    a = length(raw_pcm);
-    b = length(enc_pcm);
-    range = min(a,b);
     % ffmpeg adds an offset to transcoded data at the start
-    ffmepg_offset = 2048;
+    offset = 2048;
+    a = length(raw_pcm);
+    b = length(enc_pcm)-offset;
+    % transcoded PCM is longer, compare only available length
+    range = min(a, b);
+    
+    original_signal = raw_pcm(1:range);
+    encodec_signal = enc_pcm(offset:(offset+range-1));
+    
+    % difference
+    delta = abs(encodec_signal - original_signal);
+    score = sum(delta);
 
-    score = 0;
-    delta = zeros(1, range);
-    for i=1:range
-        % squared difference
-        delta(i) = enc_pcm(i+ffmepg_offset) - raw_pcm(i);
-        score = score + delta(i)*delta(i);
-    end
-
-%     plot(1:range, delta, 'b');
+%     plot(original_signal, 'b');
 %     hold on;
-%     plot(1:range, raw_data(1:range), 'r');
-%     hold on;
-%     x = ffmepg_offset;
-%     y = range+ffmepg_offset-1;
-%     plot(1:range, transcoded_data(x:y), 'g');
+%     plot(delta, 'r');
+% %     hold on;
+% %     plot(encodec_signal, 'g');
 end
