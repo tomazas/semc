a9c215ec4d97c79a7f867f2303539e154b377315
Darius Sabaliauskas <Sabaliauskas.Darius@gmail.com>
2013-09-23 22:13:31 +0300

    raw cpm comparison changed to mean square error comparison

--- Analysis/modules/sq_diff_analyser.m
+++ Analysis/modules/sq_diff_analyser.m
@@ -1,16 +1,16 @@
-% compares PCM streams dirrectly by taking a difference of the
-% sample data and summing the result as score, lower score is better (0 = PCMs match perfectly) 
-function [score desc] = sq_diff_analyser(raw_pcm, enc_pcm)
+% compares PCM streams dirrectly by calculating Mean Square Error of two
+% signals 
+function [mse desc] = sq_diff_analyser(raw_pcm, enc_pcm)
 
     desc = {};
 
     desc.FILE_PATTERN    =  'diff-%s.png';
     desc.Y_TITLE         =  'Score (lower is better)';
-    desc.X_TITLE_PATTERN =  'Difference analysis - %s';
+    desc.X_TITLE_PATTERN =  'Mean Square Error analysis - %s';
     
     % difference
-    delta = abs(enc_pcm - raw_pcm);
-    score = sum(delta);
+    delta = abs(enc_pcm - raw_pcm).^2;
+    mse = sum(delta(:))/numel(raw_pcm);
 
 %     plot(raw_pcm, 'b');
 %     hold on;
